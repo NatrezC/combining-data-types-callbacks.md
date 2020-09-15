0cf123bc9eed7e5de5c8f360efe4eb014b2e00f9
NatrezC <cnatrez@gmail.com>
2020-09-15 08:57:56 -0500

    finished arrays with callbacks

--- arrays_w_callbacks.js
+++ arrays_w_callbacks.js
@@ -79,3 +79,101 @@ foo();*/
 //------------------------------------------------------------------------
 
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
+
+const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
+
+//Every-------------
+//Determine if every number is greater than or equal to 0
+const greaterNum = nums.map((index) => {
+    return index >= 0;
+})
+
+//determine if every word shorter than 8 characters
+const shorter = (word) => {
+    return word.length < 8;
+}
+panagram.map(shorter)
+
+//Filter--------------
+//Filter the array for numbers less than 4
+const lessFour = nums.filter((index) => {
+    return index < 4;
+})
+console.log(lessFour)
+
+//Filter words that have an even length
+const newArray = panagram.filter(function (word) {
+    return word.length % 2 === 0;
+})
+
+console.log(newArray)
+
+//Find-----------------
+//Find the first value divisible by 5
+const find = nums.find((index) => {
+    return index % 5 === 0;
+})
+console.log(find)
+
+//find the first word that is longer than 5 characters
+const found = panagram.find(function (word) {
+    return word.length > 5
+})
+console.log(found) //i cant figure this one out
+//Find Index--------------
+// find the index of the first number that is divisible by 3
+const divisiabeThree = nums.findIndex((index) => {
+    return nums % 3 === 0
+})
+console.log(divisiabeThree)
+
+// find the index of the first word that is less than 2 characters long
+const longTwo = panagram.findIndex(function (word) {
+    return word.length < 2
+})
+console.log(longTwo) // no word less than two characters
+
+// For Each---------------
+// console.log each value of the nums array multiplied by 3
+nums.forEach(index => console.log(index * 3))
+
+// console.log each word with an exclamation point at the end of it
+panagram.forEach(word => console.log(word + "!"))
+
+// Thought Questions----------
+
+// What happened to the original array ?
+//     Can you store the values from a forEach method in a new array ? 
+
+//Map--------------------------
+// make a new array of each number multiplied by 100
+const hundred = nums.map((index) => {
+    return index * 100
+})
+console.log(hundred)
+// make a new array of all the words in all uppercase
+const toUpper = panagram.map((word) => {
+    return word.toUpperCase()
+})
+console.log(toUpper)
+
+// Thought Questions-------------
+
+// What happened to the original array ?
+//     Can you store the values from a map method in a new array ?
+//Some-------------------------------
+// Find out if some numbers are divisible by 7
+const divSeven = nums.some((index) => {
+    return index % 7 === 0
+})
+console.log(divSeven)
+// Find out if some words have the letter a in them
+const letterA = panagram.some((word) => {
+    const aye = word.toString()
+    return aye.includes('a')
+})
+console.log(letterA)
+
+
+
